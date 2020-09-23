72d0d4b2ee83765e54104294530b6f7aad5c1da3
bucholga <buchatskaya_olga_112@mail.ru>
2020-09-23 20:05:38 +0300

    Lab-1. First try to pass task1.

---
+++ Image_recognition/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using Image_recog_lib;
+
+namespace Image_recognition
+{
+    class Program
+    {
+        static void Pred(object sender, EventArgs e)
+        {
+            string item;
+            ((ConcurrentQueue<string>)sender).TryDequeue(out item);
+            Console.WriteLine(item);
+        }
+
+        static void Main(string[] args)
+        {
+            string dirpath, modelpath;
+
+            Console.WriteLine("Please enter images directory");
+            dirpath = Console.ReadLine();
+
+            Console.WriteLine("Please enter model directory");
+            modelpath = Console.ReadLine();
+
+
+
+            Recognize recon = new Recognize(modelpath);
+            recon.Notify += Pred;
+            recon.ParallelProcess(dirpath);
+            
+
+        }
+  
+    }
+}
