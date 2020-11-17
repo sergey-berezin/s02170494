30beed8afc3ac2afade70ec57bb87d71d9b8aefd
bucholga <buchatskaya_olga_112@mail.ru>
2020-11-17 15:07:45 +0300

    Lab-3. DataBase added.

---
+++ UserInterfaceRecognition/ClassInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserInterfaceRecognition
+{
+    public class ClassInfo
+    {
+        [Key]
+        public string Name { get; set; }
+        public List<RecognitionModel> RecogModel { get; set; }
+        public ClassInfo(string name = "", RecognitionModel rec = null)
+        {
+            this.Name = name;
+            this.RecogModel = new List<RecognitionModel>();
+            this.RecogModel.Add(rec);
+        }
+        public ClassInfo()
+        { }
+
+
+    }
+}
--- UserInterfaceRecognition/RecognitionViewModel.cs
+++ UserInterfaceRecognition/RecognitionViewModel.cs
@@ -10,6 +10,9 @@ using System.Windows.Forms;
 using ImageRecognitionLibrary;
 using System.ComponentModel;
 using System.Threading.Tasks;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace UserInterfaceRecognition
 {
@@ -19,12 +22,14 @@ namespace UserInterfaceRecognition
         public event PropertyChangedEventHandler PropertyChanged;
         public Commands StartRecognitionCommand { get; set; }
         public Commands CancelRecognitionCommand { get; set; }
+        public Commands ClearDataBaseCommand { get; set; }
+        public Commands StatisticCommand { get; set; }
         public ObservableCollection<Tuple<string, int>> ClassObserv { get; set; }
         public ObservableCollection<RecognitionModel> Observ { get; set; }
         public ObservableCollection<RecognitionModel> SelectedClassObserv { get; set; }
         public Recognize Recon { get; set; }
         public Dispatcher Dispatcherr { get; set; }
-
+        public Context DataBaseContext { get; set; }
         public RecognitionViewModel()
         {
             this.Observ = new ObservableCollection<RecognitionModel>();
@@ -33,9 +38,12 @@ namespace UserInterfaceRecognition
             this.Recon = new Recognize();
             this.Recon.Notify += OnPredictionCome;
             this.Dispatcherr = Dispatcher.CurrentDispatcher;
+            this.DataBaseContext = new Context();
 
             this.StartRecognitionCommand = new Commands(Start);
             this.CancelRecognitionCommand = new Commands(Cancel);
+            this.ClearDataBaseCommand = new Commands(Clear);
+            this.StatisticCommand = new Commands(Statistic);
         }
 
         private void OnPropertyChanged(string propertyName)
@@ -64,7 +72,7 @@ namespace UserInterfaceRecognition
             }
         }
 
-        private  void OnPredictionCome(PredictionResult pr, EventArgs e)
+        private  void OnPredictionCome(PredictionResult pr, EventArgs e, bool isInBase = false)
         {
 
             Dispatcherr.BeginInvoke(DispatcherPriority.Background,new Action(() =>
@@ -80,9 +88,30 @@ namespace UserInterfaceRecognition
                     }
                 }
                 if (index != -1)
+                {
                     ClassObserv[index] = new Tuple<string, int>(ClassObserv[index].Item1, ClassObserv[index].Item2 + 1);
+
+                }
                 else
                     ClassObserv.Add(new Tuple<string, int>(pr.ClassLabel, 1));
+                if (!isInBase)
+                {
+                    var newElem = new RecognitionModel(pr.Path, pr.ClassLabel);
+                    DataBaseContext.DataBaseInfo.Add(newElem);
+                    DataBaseContext.SaveChanges();
+                    if (DataBaseContext.ClassLabelsInfo.Find(newElem.ClassLabel) == null)
+                    {
+                        DataBaseContext.ClassLabelsInfo.Add(new ClassInfo(newElem.ClassLabel, newElem));
+                    }
+                    else
+                    {
+                        DataBaseContext.ClassLabelsInfo.Find(newElem.ClassLabel).RecogModel.Add(newElem);
+                    }
+                    DataBaseContext.SaveChanges();
+                   
+                }
+
+
             }));
             
         }
@@ -95,24 +124,75 @@ namespace UserInterfaceRecognition
                 ClassObserv.Clear();
                 Observ.Clear();
                 SelectedClassObserv.Clear();
-                Recognition(fbd.SelectedPath); 
+                DirectoryInfo dirInfo = new DirectoryInfo(fbd.SelectedPath);
+                FileInfo[] files = dirInfo.GetFiles("*.jpg");
+                List<string> imagesNotInBase = new List<string>();
+                var tmp = DataBaseContext.DataBaseInfo.Include(p => p.Image);
+                foreach (var item in files)
+                {
+                    foreach (var it in tmp.Where(p => p.Path == item.FullName && p.Image.ImageBlob.SequenceEqual(File.ReadAllBytes(item.FullName))))
+                    {
+                        PredictionResult findPred = new PredictionResult(it.Path, it.ClassLabel);
+                        OnPredictionCome(findPred, null, true);
+                    }
+                    
+                    if (!tmp.Any(p => p.Path == item.FullName && p.Image.ImageBlob.SequenceEqual(File.ReadAllBytes(item.FullName))))
+                    {
+                        imagesNotInBase.Add(item.FullName);
+                    }
+                }
+
+                Recognition(imagesNotInBase); 
                 
             }
         }
 
-        private void Recognition(string path)
+        private void Recognition(List<string> path)
         {
             Task.Run(() =>
                 {
                     Recognize.cts = new CancellationTokenSource();
                     Recon.ParallelProcess(path);
+                    this.Dispatcherr.BeginInvoke(new Action(() => { DataBaseContext.SaveChanges(); }));
                 });
+            
         }
 
         private void Cancel(object sender)
         {
             this.Recon.Stop();
         }
+        
+        private void Clear(object sender)
+        {
+            foreach (var item in DataBaseContext.DataBaseInfo)
+            {
+                DataBaseContext.DataBaseInfo.Remove(item);
+            }
+            foreach (var item in DataBaseContext.ClassLabelsInfo)
+            {
+                DataBaseContext.ClassLabelsInfo.Remove(item);
+            }
+            DataBaseContext.SaveChanges();
+            ClassObserv.Clear();
+            Observ.Clear();
+            SelectedClassObserv.Clear();
+
+        }
+
+        private void Statistic(object sender)
+        {
+            StatisticInfo statistic = new StatisticInfo();
+            foreach (var item in DataBaseContext.ClassLabelsInfo.Include(p => p.RecogModel))
+            {
+                statistic.stat.Add(item.Name, item.RecogModel.Count);
+            }
+            MessageBox.Show(statistic.ToString());
+
+        }
+
+        
+
         
         
 
